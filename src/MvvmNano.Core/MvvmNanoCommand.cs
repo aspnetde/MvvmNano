6e4b5138da736dc13c12248ce8ceeba7d7b3a039
Thomas Bandt <bandt@69grad.de>
2015-12-31 00:12:36 +0100

    Adds MvvmNanoCommand, which implements ICommand ... ... and can therefore be used with data-binding.

---
+++ src/MvvmNano.Core/MvvmNanoCommand.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MvvmNano
+{
+    public class MvvmNanoCommand : ICommand
+    {
+        private readonly Func<bool> _canExecute;
+        private readonly Action _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public MvvmNanoCommand(Action execute) : this(execute, null)
+        {
+        }
+
+        public MvvmNanoCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+                handler(this, EventArgs.Empty);
+        }
+    }
+
+    public class MvvmNanoCommand<T> : ICommand
+    {
+        private readonly Func<T, bool> _canExecute;
+        private readonly Action<T> _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public MvvmNanoCommand(Action<T> execute) : this(execute, null)
+        {
+        }
+
+        public MvvmNanoCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+                handler(this, EventArgs.Empty);
+        }
+    }
+}
+
