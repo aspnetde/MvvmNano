b2818e52437d64b9c537d9a637970b22abcb27c8
Thomas Bandt <bandt@69grad.de>
2015-12-30 17:34:26 +0100

    Adds MvvmNanoContentPage

---
+++ src/MvvmNano.Forms/MvvmNanoContentPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Linq.Expressions;
+
+namespace MvvmNano.Forms
+{
+    public abstract class MvvmNanoContentPage<TViewModel> : ContentPage, IView
+        where TViewModel : IViewModel
+    {
+        protected TViewModel ViewModel
+        {
+            get { return (TViewModel)BindingContext; }
+        }
+
+        protected void BindToViewModel(BindableObject self, BindableProperty targetProperty, 
+            Expression<Func<TViewModel, object>> sourceProperty, BindingMode mode = BindingMode.Default, 
+            IValueConverter converter = null, string stringFormat = null)
+        {
+            self.SetBinding(targetProperty, sourceProperty, mode, converter, stringFormat);
+        }
+
+        public void SetViewModel(IViewModel viewModel)
+        {
+            BindingContext = viewModel;
+        }
+    }
+}
+
