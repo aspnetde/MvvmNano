1540cc3b6c511e62ed0d5b2ba8c51ddaabd32d9a
Thomas Bandt <bandt@69grad.de>
2016-01-03 00:26:54 +0100

    Introducing MvvmNanoNavigationPage A custom navigation page which makes sure to clean up when pages are popped from the stack.

---
+++ src/MvvmNano.Forms/MvvmNanoNavigationPage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MvvmNano.Forms
+{
+    public class MvvmNanoNavigationPage : NavigationPage
+    {
+        public MvvmNanoNavigationPage(Page root) : base(root)
+        {
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Popped += PagePopped;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            Popped -= PagePopped;
+        }
+
+        private static void PagePopped(object sender, NavigationEventArgs e)
+        {
+            var disposablePage = e.Page as IDisposable;
+
+            if (disposablePage != null)
+                disposablePage.Dispose();
+        }
+    }
+}
+
