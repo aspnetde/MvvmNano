c619e4bff63069d9512b44cd695ecff506cb0600
Thomas Bandt <bandt@69grad.de>
2016-01-28 22:36:37 +0100

    Updates Assembly Infos

--- src/MvvmNano.Core/Properties/AssemblyInfo.cs
+++ src/MvvmNano.Core/Properties/AssemblyInfo.cs
@@ -1,27 +1,8 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("MvvmNano.Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyDescription("The small and smart MVVM framework made with ❤ for Xamarin.Forms.")]
+[assembly: AssemblyProduct("MvvmNano")]
 [assembly: AssemblyCopyright("Thomas Bandt")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("0.1.*")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+[assembly: AssemblyVersion("1.0.0")]
