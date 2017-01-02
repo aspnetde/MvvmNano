1ca19fdf6b108053f1ed35e4e48b61fda86cd282
Thomas Bandt <bandt@69grad.de>
2017-01-02 20:19:45 +0100

    Introduces MvvmNano.Ninject

---
+++ src/MvvmNano.Ninject/MvvmNanoNinjectAdapter.cs
@@ -0,0 +1,37 @@
+﻿using Ninject;
+
+namespace MvvmNano.Ninject
+{
+    public class MvvmNanoNinjectAdapter : IMvvmNanoIoCAdapter
+    {
+        private readonly StandardKernel _kernel;
+
+        public MvvmNanoNinjectAdapter()
+        {
+            _kernel = new StandardKernel();
+        }
+
+        public void Register<TInterface, TImplementation>() where TImplementation : TInterface
+        {
+            _kernel.Unbind<TInterface>();
+            _kernel.Bind<TInterface>().To<TImplementation>();
+        }
+
+        public void RegisterAsSingleton<TInterface, TImplementation>() where TImplementation : TInterface
+        {
+            _kernel.Unbind<TInterface>();
+            _kernel.Bind<TInterface>().To<TImplementation>().InSingletonScope();
+        }
+
+        public void RegisterAsSingleton<TInterface>(TInterface instance)
+        {
+            _kernel.Unbind<TInterface>();
+            _kernel.Bind<TInterface>().ToConstant(instance);
+        }
+
+        public TInterface Resolve<TInterface>()
+        {
+            return _kernel.Get<TInterface>();
+        }
+    }
+}
