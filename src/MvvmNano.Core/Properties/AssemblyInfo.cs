eaa8951a1bde542e46ef52d1772665e5a1a22817
Konrad Müller <k.l.mueller@outlook.com>
2017-12-07 07:44:18 +0100

    Update to netstandard2.0

--- src/MvvmNano.Core/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("MvvmNano.Core")]
-[assembly: AssemblyDescription("The small and smart MVVM framework made with ❤ for Xamarin.Forms.")]
-[assembly: AssemblyProduct("MvvmNano")]
-[assembly: AssemblyCopyright("Thomas Bandt")]
-
-[assembly: AssemblyVersion("3.1.0")]
