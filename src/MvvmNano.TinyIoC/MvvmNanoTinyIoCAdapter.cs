f8fbdd1e5f85d47bb4e87df3ba623e86c7df5412
Konrad Müller <k.l.mueller@outlook.com>
2017-12-07 07:44:04 +0100

    Added tiny ioc adapter for netstandard

---
+++ src/MvvmNano.TinyIoC/MvvmNanoTinyIoCAdapter.cs
@@ -0,0 +1,37 @@
+﻿using TinyIoC;
+
+namespace MvvmNano.TinyIoC
+{
+    /// <summary>
+    /// MvvmNano IoC adapter that is using TinyIoC. 
+    /// </summary>
+    public class MvvmNanoTinyIoCAdapter : IMvvmNanoIoCAdapter
+    {
+        private readonly TinyIoCContainer _container;
+
+        public MvvmNanoTinyIoCAdapter()
+        {
+            _container = new TinyIoCContainer();
+        }
+
+        public void Register<TInterface, TImplementation>() where TImplementation : TInterface
+        {
+            _container.Register<TInterface, TImplementation>();
+        }
+
+        public void RegisterAsSingleton<TInterface, TImplementation>() where TImplementation : TInterface
+        {
+            _container.Register<TInterface, TImplementation>().AsSingleton();
+        }
+
+        public void RegisterAsSingleton<TInterface>(TInterface instance)
+        {
+            _container.Register<TInterface>(instance);
+        }
+
+        public TInterface Resolve<TInterface>()
+        {
+            return _container.Resolve<TInterface>();
+        }
+    }
+}
